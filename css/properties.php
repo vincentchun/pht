917d4ee8256a3e8cdb7c72adae67370a33481516
vincentchun <83322248+vincentchun@users.noreply.github.com>
2024-02-27 11:26:12 +1100

    Update properties.php

--- css/properties.php
+++ css/properties.php
@@ -1,4 +1,12 @@
 class CSSProperty {
  public $name;
  public $type;
+ public function __construct($name, $type){
+  $this->name = $name;
+  $this->type = $type;
  }
+}
+
+$properties = array(
+ "background-color" => new CSSProperty("background-color","color"),
+);
